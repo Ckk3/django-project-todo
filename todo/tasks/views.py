936fef1f05ea57b0639e080156b7a6f4390c8390
Ckk3 <contato.gustavomota@gmail.com>
2022-11-22 11:01:39 -0300

    Create option to select every task page and see description

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from .models import Task
@@ -15,3 +15,7 @@ def task_list(request):
 
 def your_name(request, name):
     return render(request, 'tasks/your_name.html', {'name': name})
+
+def task_view(request, id):
+    task = get_object_or_404(Task, pk=id)
+    return render(request, 'tasks/task_view.html', {'task': task})
